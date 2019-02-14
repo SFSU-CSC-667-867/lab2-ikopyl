6274334c17cd13091abf7a9d0a6965b6e87c5130
Ilya Kopyl <ilya.kopyl@gmail.com>
2019-02-13 21:16:37 -0800

    Making app2 talk to app, with query

--- app2.js
+++ app2.js
@@ -9,6 +9,17 @@ server.get('/world', (req, res) => {
     res.send('<h1>Hello, Network!</h1>');
 });
 
+server.get('/network', (req, res) => {
+    console.log(req.query)
+    axios.get(`http://localhost:3000/hello?name=${req.query.name}`)
+    .then((networkResponse) => {
+        res.send(networkResponse.data);
+    })
+    .catch(() => {
+        res.send(500, '<h3>HTTP 500 Error: Unable to connect to localhost:3000</h3>');
+    });
+});
+
 server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}!`);
 });
